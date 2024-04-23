4f575d7dfb2cc67ad76407a3337659ee7898843b
Rohith Jayawardene <gambol99@gmail.com>
2024-04-23 15:29:33 +0100

    chore: renaming the filetype and bumping the version of alarms

--- alarms.tf
+++ alarms.tf
@@ -6,7 +6,7 @@
 module "alarm_baseline" {
   count   = var.enable_cis_alarms ? 1 : 0
   source  = "appvia/alarm-baseline/aws"
-  version = "0.0.3"
+  version = "0.1.0"
 
   enable_iam_changes                  = false
   enable_mfa_console_signin_allow_sso = true
