b06dfe16978e62235c67da678ba365306cf8127f
Rohith Jayawardene <gambol99@gmail.com>
2024-04-24 13:30:15 +0100

    fix: bumping the version of the alarms and indirectly the notifications module

--- alarms.tf
+++ alarms.tf
@@ -6,7 +6,7 @@
 module "alarm_baseline" {
   count   = var.enable_cis_alarms ? 1 : 0
   source  = "appvia/alarm-baseline/aws"
-  version = "0.1.2"
+  version = "0.1.3"
 
   enable_iam_changes                  = false
   enable_mfa_console_signin_allow_sso = true
