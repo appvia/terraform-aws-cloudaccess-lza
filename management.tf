36c94ed23a0f3a770f9bb129d1e42be8138ef946
Rohith Jayawardene <gambol99@gmail.com>
2024-09-23 22:50:27 +0100

    feat: making the ammendments to the code approval and contributor roles for s3 configuration (#51)
    * feat: making the ammendments to the code approval and contributor roles for s3 configuration
    
    * chore: adding the templates to the release role as well

--- management.tf
+++ management.tf
@@ -16,8 +16,11 @@ resource "aws_iam_policy" "user_management" {
 resource "aws_iam_policy" "code_contributor" {
   name        = "lza-code-contributor"
   description = "Provides the permissions to validate the landing zone code"
-  policy      = file("${path.module}/assets/policies/code-contributor.json")
-  tags        = var.tags
+  policy = templatefile("${path.module}/assets/policies/code-contributor.json", {
+    management_account_id = local.management_account_id
+    region                = local.region
+  })
+  tags = var.tags
 
   provider = aws.management
 }
@@ -26,8 +29,11 @@ resource "aws_iam_policy" "code_contributor" {
 resource "aws_iam_policy" "code_release" {
   name        = "lza-code-release"
   description = "Provides the permissions to release the landing zone code"
-  policy      = file("${path.module}/assets/policies/code-release.json")
-  tags        = var.tags
+  policy = templatefile("${path.module}/assets/policies/code-release.json", {
+    management_account_id = local.management_account_id
+    region                = local.region
+  })
+  tags = var.tags
 
   provider = aws.management
 }
