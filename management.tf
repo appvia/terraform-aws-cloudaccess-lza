1e439c2aadfe880c0c23d3960ac0078a369595a8
George Taylor <george.taylor@appvia.io>
2025-01-31 14:09:12 +0000

    feat: add SecretsManagerReadWrite to identity role (#102)

--- management.tf
+++ management.tf
@@ -195,7 +195,8 @@ module "management_sso_identity" {
     "arn:aws:iam::aws:policy/AWSLambda_FullAccess",
     "arn:aws:iam::aws:policy/AWSKeyManagementServicePowerUser",
     "arn:aws:iam::aws:policy/AmazonS3FullAccess",
-    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+    "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess",
+    "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
   ]
 
   read_write_inline_policies = {
